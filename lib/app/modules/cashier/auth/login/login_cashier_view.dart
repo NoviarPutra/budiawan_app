9a00807b36bd751ea55ca89db8817248350e3c65
NoviarPutra <noviarpb@gmail.com>
2024-04-07 12:39:28 +0700

    update

--- lib/app/modules/cashier/auth/login/login_cashier_view.dart
+++ lib/app/modules/cashier/auth/login/login_cashier_view.dart
@@ -35,106 +35,122 @@ class LoginCashierView extends GetView<LoginCashierController> {
             )
           ],
         ),
-        body: Container(
-          padding: const EdgeInsets.all(20),
-          child: SingleChildScrollView(
-            child: Column(
-              children: [
-                // Text Login
-                Container(
-                  padding: const EdgeInsets.all(20),
-                  alignment: Alignment.center,
-                  child: const Column(
-                    children: [
-                      Text(
-                        'Login Cashier App',
-                        style: TextStyle(
-                          fontSize: 30,
-                          fontWeight: FontWeight.w900,
+        body: Center(
+          child: Container(
+            padding: const EdgeInsets.all(20),
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  // Text Login
+                  Container(
+                    padding: const EdgeInsets.all(20),
+                    alignment: Alignment.center,
+                    child: const Column(
+                      children: [
+                        Text(
+                          'Login Cashier App',
+                          style: TextStyle(
+                            fontSize: 30,
+                            fontWeight: FontWeight.w900,
+                          ),
                         ),
-                      ),
-                      Text(
-                        'Enter credentials to login',
-                        style: TextStyle(
-                          fontSize: 16,
+                        Text(
+                          'Enter credentials to login',
+                          style: TextStyle(
+                            fontSize: 16,
+                          ),
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
-                ),
 
-                // Form Login
-                Container(
-                  padding: const EdgeInsets.all(20),
-                  alignment: Alignment.center,
-                  child: FormBuilder(
-                    key: controller.formKey,
-                    child: SingleChildScrollView(
-                      child: Column(
-                        children: [
-                          baseInputText(
-                            label: 'Email',
-                            name: 'email',
-                            hintText: 'example@email.com',
-                            prefixIcon: const Icon(Icons.email),
-                            keyboardType: TextInputType.emailAddress,
-                            onFieldSubmitted: (_) => controller.handleLogin(),
-                            validator: FormBuilderValidators.compose([
-                              FormBuilderValidators.required(
-                                errorText: 'Email tidak boleh kosong',
-                              ),
-                              FormBuilderValidators.email(
-                                errorText: 'Email tidak valid',
-                              ),
-                            ]),
-                          ),
-                          baseInputText(
-                            label: 'Password',
-                            name: 'password',
-                            hintText: '************',
-                            obscureText: true,
-                            prefixIcon: const Icon(Icons.password),
-                            onFieldSubmitted: (_) => controller.handleLogin(),
-                            validator: FormBuilderValidators.compose([
-                              FormBuilderValidators.required(
-                                errorText: 'Password tidak boleh kosong',
-                              ),
-                            ]),
-                          ),
-                          baseElevatedButton(
-                            title: 'Login',
-                            onTap: () {
-                              controller.handleLogin();
-                            },
-                          ),
+                  // Form Login
+                  Container(
+                    padding: const EdgeInsets.all(20),
+                    alignment: Alignment.center,
+                    child: FormBuilder(
+                      key: controller.formKey,
+                      child: SingleChildScrollView(
+                        child: Column(
+                          children: [
+                            baseInputText(
+                              label: 'Email',
+                              name: 'email',
+                              hintText: 'example@email.com',
+                              prefixIcon: const Icon(Icons.email),
+                              keyboardType: TextInputType.emailAddress,
+                              onFieldSubmitted: (_) => controller.handleLogin(),
+                              validator: FormBuilderValidators.compose([
+                                FormBuilderValidators.required(
+                                  errorText: 'Email tidak boleh kosong',
+                                ),
+                                FormBuilderValidators.email(
+                                  errorText: 'Email tidak valid',
+                                ),
+                              ]),
+                            ),
+                            baseInputText(
+                              label: 'Password',
+                              name: 'password',
+                              hintText: '************',
+                              obscureText: true,
+                              prefixIcon: const Icon(Icons.password),
+                              onFieldSubmitted: (_) => controller.handleLogin(),
+                              validator: FormBuilderValidators.compose([
+                                FormBuilderValidators.required(
+                                  errorText: 'Password tidak boleh kosong',
+                                ),
+                              ]),
+                            ),
+                            baseElevatedButton(
+                              title: 'Login',
+                              onTap: () {
+                                controller.handleLogin();
+                              },
+                            ),
 
-                          // Forgot Password
-                          const SizedBox(height: 5),
-                          TextButton(
-                            onPressed: () {},
-                            child: const Text('Forgot Password?'),
-                          ),
+                            // Forgot Password
+                            const SizedBox(height: 5),
+                            TextButton(
+                              onPressed: () {
+                                showAboutDialog(
+                                  context: context,
+                                  applicationName: 'Cashier App',
+                                  applicationVersion: '1.0.0',
+                                  applicationIcon: const FlutterLogo(),
+                                  applicationLegalese:
+                                      'Copyright © ${DateTime.now().year} by Noviar Putra.',
+                                  children: [
+                                    const Text(
+                                      'Fitur ini belum tersedia. Silahkan hubungi admin.',
+                                    ),
+                                  ],
+                                );
+                              },
+                              child: const Text('Forgot Password?'),
+                            ),
 
-                          // Register
-                          const SizedBox(height: 5),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              const Text('Don\'t have an account?'),
-                              TextButton(
-                                onPressed: () {
-                                  Get.offNamed('/cashier/auth/register');
-                                },
-                                child: const Text('Register'),
-                              ),
-                            ],
-                          ),
-                        ],
+                            // Register
+                            const SizedBox(height: 5),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                const Text('Don\'t have an account?'),
+                                TextButton(
+                                  onPressed: () {
+                                    Get.offNamed('/cashier/auth/register');
+                                  },
+                                  child: const Text('Register'),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
                       ),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ),
